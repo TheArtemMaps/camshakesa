a10e844c32090dabb208cac2b66c2710a09a9069
TheArtemMaps <91487356+TheArtemMaps@users.noreply.github.com>
2023-11-10 05:50:59 +0200

    Update Main.cpp
    Added a #define to enable or disable blur replacement with camera shake

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,24 +1,38 @@
 #include "plugin.h"
 #include "MemoryMgr.h"
 #include "CCamera.h"
-
+void	HighspeedCamShake(float shake);
+void	CamShakeFunction(float shake);
+//#define REPLACEBLURWITHCAMSHAKE // When this is defined, blur gets replaced with camera shake.
 
 WRAPPER void CamShakeNoPos(CCamera* pCamera, float fStrength) { WRAPARG(pCamera); WRAPARG(fStrength); EAXJMP(0x50A970); }
 
 void PerformCamShake() {
-	CVehicle* vehicle = FindPlayerVehicle(-1, true);
-
+CVehicle* vehicle = FindPlayerVehicle(-1, true);
+#ifdef REPLACEBLURWITHCAMSHAKE
+InjectHook(0x704E8A, &HighspeedCamShake);
+InjectHook(0x704E8A, &CamShakeFunction);
+#else
 	if (vehicle && vehicle->m_nVehicleSubClass != VEHICLE_PLANE && vehicle->m_nVehicleSubClass != VEHICLE_HELI
 		&& vehicle->m_nVehicleSubClass != VEHICLE_BOAT && vehicle->m_nVehicleSubClass != VEHICLE_BMX &&
 		vehicle->m_nVehicleSubClass != VEHICLE_TRAIN) {
 		float speed = vehicle->m_vecMoveSpeed.Magnitude();
 
 		if (speed > 0.75f)
-			CamShakeNoPos(&TheCamera, FindPlayerVehicle(-1, 0)->m_vecMoveSpeed.Magnitude() * 0.080f);
+			CamShakeNoPos(&TheCamera, FindPlayerVehicle(-1, 0)->m_vecMoveSpeed.Magnitude() * 0.040f);
+
 	}
+#endif
 }
 
+void HighspeedCamShake(float shake)
+{
+	CamShakeNoPos(&TheCamera, shake * 0.030f);
+}
 
+void CamShakeFunction(float fShake) {
+	CamShakeNoPos(&TheCamera, fShake * 0.030f);
+}
 
 class CameraShake {
 public:
